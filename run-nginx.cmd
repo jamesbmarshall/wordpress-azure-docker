1ecb2b680c76c19e50a020fe3ec275b1aba39009
James Marshall <jamesbmarshall@users.noreply.github.com>
2017-06-27 13:35:39 +0100

    Add Docker scripts

---
+++ run-nginx.cmd
@@ -0,0 +1,1 @@
+docker run -d -p 80:80 -p 443:443 --name nginx-proxy -v /wordpress/nginx/certs:/etc/nginx/certs:ro -v /home/james/nginx.conf:/etc/nginx/nginx.conf:ro -v /home/james/vhost.d:/etc/nginx/vhost.d:rw -v /etc/nginx/vhost.d -v /usr/share/nginx/html -v /var/run/docker.sock:/tmp/docker.sock:ro --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy=true jwilder/nginx-proxy
