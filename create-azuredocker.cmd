941994158452da89db9ae551fd7c7d48dd4e3d52
James Marshall <jamesbmarshall@users.noreply.github.com>
2017-06-27 21:51:34 +0100

    Ports Update
    Remove unnecessary port configuration on Azure VM creation.

--- create-azuredocker.cmd
+++ create-azuredocker.cmd
@@ -1,4 +1,4 @@
 @echo off
 echo Creating Docker host using docker-machine in Azure (North Europe) Azure-Live...
 set AZURE_SUBSCRIPTION="YOUR SUBSCRIPTION ID"
-docker-machine create --driver azure --azure-subscription-id %AZURE_SUBSCRIPTION% --azure-image Canonical:UbuntuServer:16.04.0-LTS:latest --azure-location northeurope  --azure-size Standard_A1 --azure-ssh-user james --azure-resource-group Azure-Live --azure-open-port 80 --azure-open-port 22 --azure-open-port 443 --azure-open-port 15672 --azure-open-port 5672 docker-host
+docker-machine create --driver azure --azure-subscription-id %AZURE_SUBSCRIPTION% --azure-image Canonical:UbuntuServer:16.04.0-LTS:latest --azure-location northeurope  --azure-size Standard_A1 --azure-ssh-user james --azure-resource-group Azure-Live --azure-open-port 80 --azure-open-port 22 --azure-open-port 443 docker-host
